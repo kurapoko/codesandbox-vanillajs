debbc2d1f7145f9727ce707f6cd44f3e5731b4e6
kurapoko <kuraharamasahiro@gmail.com>
2020-03-21 20:55:22 +0900

    オブジェクト指向

--- src/index.js
+++ src/index.js
@@ -6,5 +6,20 @@ document.getElementById("app").innerHTML = `
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
-<div></div>
 `;
+
+var Member = function(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+};
+
+var mem = new Member("Masahiro", "Kurahara");
+mem.getName = function() {
+  return this.lastName + " " + this.firstName;
+};
+
+console.log(mem.getName());
+
+var mem2 = new Member("倉原", "昌宏");
+
+console.log(mem2.firstName);
