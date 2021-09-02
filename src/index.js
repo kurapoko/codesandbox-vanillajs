aa146872ae64f94a453ca5ded752c3c0d814ae37
kurapoko <kuraharamasahiro@gmail.com>
2021-09-03 08:14:59 +0900

    非同期処理

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<div class="textCounter">
-  <textarea id="textCounter">
-  </textarea>
-  <p><span id="count">0</span>/<span id="max"></span></p>
-</div>
+
 `;
 
-const textWrapper = document.querySelector(".textCounter");
-const textArea = document.querySelector("#textCounter");
-const count = document.querySelector("#count");
-const max = document.querySelector("#max");
-const maxLength = 8;
-max.textContent = maxLength;
+console.log("SetTimeoutの前" + new Date());
 
-textArea.addEventListener("input", function () {
-  count.textContent = textArea.value.length;
-  if (textArea.value.length > maxLength) {
-    textWrapper.classList.add("error");
-  } else {
-    textWrapper.classList.remove("error");
-  }
-});
+function f() {
+  console.log("これは関数Fの中：" + new Date());
+}
+
+setTimeout(f(), 1000);
